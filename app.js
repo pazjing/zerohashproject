3dbd5bb97d0ffda6aad90768334dbcd9661ea385
pazjing <pazjing@hotmail.com>
2022-02-13 14:19:18 +1300

    improve nodejs function

--- app.js
+++ app.js
@@ -2,42 +2,37 @@ var express = require("express");
 var app = express();
 var request = require('request');
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
-
 /* GET home page. */
 app.get('/', function(req, res, next) {
-    res.send("Home page. 200 OK");
-    res.status(200);
+    res.status(200).json({"message": "Home page response with 200 OK"});
   });
 
 app.get('/health', function(req, res, next) {
-    res.send("Running fine. 200 OK");
-    res.status(200);
+    res.status(200).json({"message": "Application is running OK"});
   });
 
 app.get('/:currency', async function (req, res) {
     reqCurrency = req.params.currency.toUpperCase();
     var currencyList = ["EUR","GBP","USD","JPY"];
-  
+    //res.setTimeout(600);
+
     if (currencyList.includes(reqCurrency)) {
-      currencyApiUrl = 'https://api.coinbase.com/v2/prices/spot?currency=' + reqCurrency
+      currencyApiUrl = 'https://api.coinbase.com/v2/prices/spot?currency=' + reqCurrency;
+      console.log(currencyApiUrl);
       request.get(
         currencyApiUrl,
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body);
                 const response = body;
-                res.send(response);
+                res.status(200).json(response);
+                //res.send(response);
             }
         }
       );
     } 
     else {
-      var errorResponse = "Please only use EUR, GBP, USD, JPY for the current valid currency request.";
-      res.status(404);
-      res.send(errorResponse);
+        res.status(400).json({"error": "Currently, only support EUR, GBP, USD, JPY currencies"});
     }
 })
 
